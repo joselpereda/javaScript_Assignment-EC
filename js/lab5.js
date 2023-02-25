d4cfa196f395d683de3606739200f470048f73d3
joselpereda <joselpereda@hotmail.com>
2023-02-24 22:41:24 -0800

    added the correct method in line 4: toLowerCase()

--- js/lab5.js
+++ js/lab5.js
@@ -1,7 +1,7 @@
 // Prompt user for Y, y, N or n response
 do {
     // If renponse is not valid, continue prompting
-    again = prompt("repeat entries? (y or n)" ,"y").toLocaleUpperCase();
+    again = prompt("repeat entries? (y or n)" ,"y").toLowerCase();
 } while (!((again === "y") || (again === "n")));
 
 document.write(`Response was ${again}`);
